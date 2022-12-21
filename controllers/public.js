5b1c8cff56cc97de418f32f858cf13e7a1a2a0ab
Luciano <echevarneluciano@gmail.com>
2022-12-21 18:34:49 -0300

    otro

--- controllers/public.js
+++ controllers/public.js
@@ -1,3 +1,37 @@
+var { Img, Posteo, User, Like, Comentario } = require("../models");
+const sequelize = require("sequelize");
+
 exports.muro = exports.muro = async function (req, res) {
-  res.render("publicMuro");
+  let posts = await Posteo.findAll({
+    where: { tipo: "Publico" },
+    include: [{ model: Img }, { model: User }, { model: Comentario }],
+  });
+
+  let promedio = await Posteo.findAll({
+    attributes: ["id"],
+    raw: true,
+    include: [
+      {
+        model: Like,
+        attributes: [
+          [sequelize.fn("avg", sequelize.col("estrellas")), "promedio"],
+        ],
+        required: true,
+      },
+    ],
+    group: ["posteo.id"],
+  });
+  posts.map((p) => {
+    p.dataValues.promedio = "datos insuficientes";
+    promedio.map((pro) => {
+      if (p.id == pro.id) {
+        p.dataValues.promedio = pro["Likes.promedio"];
+      }
+    });
+  });
+
+  res.render("publicMuro", {
+    title: "Fotasa App",
+    posts: posts,
+  });
 };
