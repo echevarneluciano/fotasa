95a1c2dc7b2ae1607d1d47fa3ac45d3d47a927d6
Luciano <echevarneluciano@gmail.com>
2023-01-01 14:44:08 -0300

    final

--- controllers/public.js
+++ controllers/public.js
@@ -50,6 +50,37 @@ exports.buscar = async function (req, res) {
   };
 
   let buscador = await Posteo.findAll(options);
+
+  let promedio = await Posteo.findAll({
+    attributes: ["id"],
+    raw: true,
+    where: {
+      [Op.or]: [
+        { titulo: { [Op.like]: "%" + req.body.palabraclave + "%" } },
+        { etiquetas: { [Op.like]: "%" + req.body.palabraclave + "%" } },
+      ],
+      [Op.and]: [{ tipo: "Publico" }],
+    },
+    include: [
+      {
+        model: Like,
+        attributes: [
+          [sequelize.fn("avg", sequelize.col("estrellas")), "promedio"],
+        ],
+        required: true,
+      },
+    ],
+    group: ["posteo.id"],
+  });
+  buscador.map((p) => {
+    p.dataValues.promedio = "datos insuficientes";
+    promedio.map((pro) => {
+      if (p.id == pro.id) {
+        p.dataValues.promedio = pro["Likes.promedio"];
+      }
+    });
+  });
+
   res.render("buscadosPublic", {
     title: "Fotasa App",
     posts: buscador,
--- controllers/search.js
+++ controllers/search.js
@@ -1,5 +1,6 @@
-var { Img, Posteo, User, Comentario } = require("../models");
+var { Img, Posteo, User, Comentario, Like } = require("../models");
 const { Op } = require("sequelize");
+const sequelize = require("sequelize");
 
 exports.buscar = async function (req, res) {
   var options = {
@@ -13,6 +14,37 @@ exports.buscar = async function (req, res) {
   };
 
   let buscador = await Posteo.findAll(options);
+
+  let promedio = await Posteo.findAll({
+    attributes: ["id"],
+    raw: true,
+    where: {
+      [Op.or]: [
+        { titulo: { [Op.like]: "%" + req.body.palabraclave + "%" } },
+        { etiquetas: { [Op.like]: "%" + req.body.palabraclave + "%" } },
+      ],
+    },
+    include: [
+      {
+        model: Like,
+        attributes: [
+          [sequelize.fn("avg", sequelize.col("estrellas")), "promedio"],
+        ],
+        required: true,
+      },
+    ],
+    group: ["posteo.id"],
+  });
+
+  buscador.map((p) => {
+    p.dataValues.promedio = "datos insuficientes";
+    promedio.map((pro) => {
+      if (p.id == pro.id) {
+        p.dataValues.promedio = pro["Likes.promedio"];
+      }
+    });
+  });
+
   res.render("buscados", {
     title: "Fotasa App",
     posts: buscador,
